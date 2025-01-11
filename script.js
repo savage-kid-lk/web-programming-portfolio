4435bbc8511ee7ec3562e8226bac23b5f958bcc0
savage-kid-lk <kekanaletago58@gmail.com>
2025-01-11 20:54:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function learnASkill() {
+  if (document.getElementById('learnJava')) {
+    console.log("learn Java");
+  }else if (document.getElementById('learnHTML')) {
+    console.log("learnHTML");
+  }
+}

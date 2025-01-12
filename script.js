47fcbce382e2c1a31920b2861024507f3a162f32
savage-kid-lk <kekanaletago58@gmail.com>
2025-01-12 03:24:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,57 @@
-function learnASkill() {
-  if (document.getElementById('learnJava')) {
-    console.log("learn Java");
-  }else if (document.getElementById('learnHTML')) {
-    console.log("learnHTML");
+function anchorFunctionality(imgId) {
+  if (imgId === 'learnJava') {
+    document.querySelector('.learnJavaDiv').style.display = 'block';
+    document.querySelector('.learnHTMLDiv').style.display = 'none';
+    document.querySelector('.learnCSSDiv').style.display = 'none';
+    document.querySelector('.learnJavaScriptDiv').style.display = 'none';
+    document.querySelector('.learnPythonDiv').style.display = 'none';
+    document.querySelector('.learnMySQLDiv').style.display = 'none';
+    document.querySelector('#clickLinkInstruction').style.display = 'block';
+    document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
+  } else if (imgId === 'learnHTML') {
+    document.querySelector('.learnJavaDiv').style.display = 'none';
+    document.querySelector('.learnHTMLDiv').style.display = 'block';
+    document.querySelector('.learnCSSDiv').style.display = 'none';
+    document.querySelector('.learnJavaScriptDiv').style.display = 'none';
+    document.querySelector('.learnPythonDiv').style.display = 'none';
+    document.querySelector('.learnMySQLDiv').style.display = 'none';
+    document.querySelector('#clickLinkInstruction').style.display = 'block';
+    document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
+  } else if (imgId === 'learnCSS') {
+    document.querySelector('.learnJavaDiv').style.display = 'none';
+    document.querySelector('.learnHTMLDiv').style.display = 'none';
+    document.querySelector('.learnCSSDiv').style.display = 'block';
+    document.querySelector('.learnJavaScriptDiv').style.display = 'none';
+    document.querySelector('.learnPythonDiv').style.display = 'none';
+    document.querySelector('.learnMySQLDiv').style.display = 'none';
+    document.querySelector('#clickLinkInstruction').style.display = 'block';
+    document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
+  } else if (imgId === 'learnJavascript') {
+    document.querySelector('.learnJavaDiv').style.display = 'none';
+    document.querySelector('.learnHTMLDiv').style.display = 'none';
+    document.querySelector('.learnCSSDiv').style.display = 'none';
+    document.querySelector('.learnJavaScriptDiv').style.display = 'block';
+    document.querySelector('.learnPythonDiv').style.display = 'none';
+    document.querySelector('.learnMySQLDiv').style.display = 'none';
+    document.querySelector('#clickLinkInstruction').style.display = 'block';
+    document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
+  } else if (imgId === 'learnPython') {
+    document.querySelector('.learnJavaDiv').style.display = 'none';
+    document.querySelector('.learnHTMLDiv').style.display = 'none';
+    document.querySelector('.learnCSSDiv').style.display = 'none';
+    document.querySelector('.learnJavaScriptDiv').style.display = 'none';
+    document.querySelector('.learnPythonDiv').style.display = 'block';
+    document.querySelector('.learnMySQLDiv').style.display = 'none';
+    document.querySelector('#clickLinkInstruction').style.display = 'block';
+    document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
+  } else if (imgId === 'learnMysql') {
+    document.querySelector('.learnJavaDiv').style.display = 'none';
+    document.querySelector('.learnHTMLDiv').style.display = 'none';
+    document.querySelector('.learnCSSDiv').style.display = 'none';
+    document.querySelector('.learnJavaScriptDiv').style.display = 'none';
+    document.querySelector('.learnPythonDiv').style.display = 'none';
+    document.querySelector('.learnMySQLDiv').style.display = 'block';
+    document.querySelector('#clickLinkInstruction').style.display = 'bblock';
+    document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
   }
 }

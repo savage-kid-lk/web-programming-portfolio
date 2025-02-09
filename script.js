a0e1845b42800265e9d53d7359908c62d1476d17
savage-kid-lk <kekanaletago58@gmail.com>
2025-02-10 01:00:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+function showMenu() {
+  document.getElementById('menuBar').style.opacity = 0;
+  document.getElementById('menuNavigation').classList.add('show');
+  event.stopPropagation();
+}
+function removeMenu() {
+  document.getElementById('menuBar').style.opacity = 1;
+  document.getElementById('menuNavigation').classList.remove('show');
+}
 function anchorFunctionality(imgId) {
   if (imgId === 'learnJava') {
+    document.querySelector('.learnLinuxDiv').style.display = 'none';
+    document.querySelector('.learnOfficeDiv').style.display = 'none';
     document.querySelector('.learnJavaDiv').style.display = 'block';
     document.querySelector('.learnHTMLDiv').style.display = 'none';
     document.querySelector('.learnCSSDiv').style.display = 'none';
@@ -9,6 +20,8 @@ function anchorFunctionality(imgId) {
     document.querySelector('#clickLinkInstruction').style.display = 'block';
     document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
   } else if (imgId === 'learnHTML') {
+    document.querySelector('.learnLinuxDiv').style.display = 'none';
+    document.querySelector('.learnOfficeDiv').style.display = 'none';
     document.querySelector('.learnJavaDiv').style.display = 'none';
     document.querySelector('.learnHTMLDiv').style.display = 'block';
     document.querySelector('.learnCSSDiv').style.display = 'none';
@@ -18,6 +31,8 @@ function anchorFunctionality(imgId) {
     document.querySelector('#clickLinkInstruction').style.display = 'block';
     document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
   } else if (imgId === 'learnCSS') {
+    document.querySelector('.learnLinuxDiv').style.display = 'none';
+    document.querySelector('.learnOfficeDiv').style.display = 'none';
     document.querySelector('.learnJavaDiv').style.display = 'none';
     document.querySelector('.learnHTMLDiv').style.display = 'none';
     document.querySelector('.learnCSSDiv').style.display = 'block';
@@ -27,6 +42,8 @@ function anchorFunctionality(imgId) {
     document.querySelector('#clickLinkInstruction').style.display = 'block';
     document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
   } else if (imgId === 'learnJavascript') {
+    document.querySelector('.learnLinuxDiv').style.display = 'none';
+    document.querySelector('.learnOfficeDiv').style.display = 'none';
     document.querySelector('.learnJavaDiv').style.display = 'none';
     document.querySelector('.learnHTMLDiv').style.display = 'none';
     document.querySelector('.learnCSSDiv').style.display = 'none';
@@ -36,6 +53,8 @@ function anchorFunctionality(imgId) {
     document.querySelector('#clickLinkInstruction').style.display = 'block';
     document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
   } else if (imgId === 'learnPython') {
+    document.querySelector('.learnLinuxDiv').style.display = 'none';
+    document.querySelector('.learnOfficeDiv').style.display = 'none';
     document.querySelector('.learnJavaDiv').style.display = 'none';
     document.querySelector('.learnHTMLDiv').style.display = 'none';
     document.querySelector('.learnCSSDiv').style.display = 'none';
@@ -45,13 +64,37 @@ function anchorFunctionality(imgId) {
     document.querySelector('#clickLinkInstruction').style.display = 'block';
     document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
   } else if (imgId === 'learnMysql') {
+    document.querySelector('.learnLinuxDiv').style.display = 'none';
+    document.querySelector('.learnOfficeDiv').style.display = 'none';
     document.querySelector('.learnJavaDiv').style.display = 'none';
     document.querySelector('.learnHTMLDiv').style.display = 'none';
     document.querySelector('.learnCSSDiv').style.display = 'none';
     document.querySelector('.learnJavaScriptDiv').style.display = 'none';
     document.querySelector('.learnPythonDiv').style.display = 'none';
     document.querySelector('.learnMySQLDiv').style.display = 'block';
-    document.querySelector('#clickLinkInstruction').style.display = 'bblock';
+    document.querySelector('#clickLinkInstruction').style.display = 'block';
+    document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
+  } else if (imgId === 'learnLinux') {
+    document.querySelector('.learnLinuxDiv').style.display = 'block';
+    document.querySelector('.learnOfficeDiv').style.display = 'none';
+    document.querySelector('.learnJavaDiv').style.display = 'none';
+    document.querySelector('.learnHTMLDiv').style.display = 'none';
+    document.querySelector('.learnCSSDiv').style.display = 'none';
+    document.querySelector('.learnJavaScriptDiv').style.display = 'none';
+    document.querySelector('.learnPythonDiv').style.display = 'none';
+    document.querySelector('.learnMySQLDiv').style.display = 'none';
+    document.querySelector('#clickLinkInstruction').style.display = 'block';
+    document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
+  }else if (imgId === 'learnOffice') {
+    document.querySelector('.learnLinuxDiv').style.display = 'none';
+    document.querySelector('.learnOfficeDiv').style.display = 'block';
+    document.querySelector('.learnJavaDiv').style.display = 'none';
+    document.querySelector('.learnHTMLDiv').style.display = 'none';
+    document.querySelector('.learnCSSDiv').style.display = 'none';
+    document.querySelector('.learnJavaScriptDiv').style.display = 'none';
+    document.querySelector('.learnPythonDiv').style.display = 'none';
+    document.querySelector('.learnMySQLDiv').style.display = 'none';
+    document.querySelector('#clickLinkInstruction').style.display = 'block';
     document.getElementById('clickLinkInstruction').scrollIntoView({ behavior: 'smooth' });
   }
 }
